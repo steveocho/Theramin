1ec93ab5b3711b1ac5d4f6b3456eaead73b8df31
jberhow <jberhow@uci.edu>
2015-11-29 21:57:23 -0800

    Create theremin.ino
    The bulk of the arduino code that will control volume and frequency

---
+++ theremin.ino
@@ -0,0 +1,43 @@
+#include "pitches.h"
+
+int trigpin = 11;
+int echopin = 12;
+int speakerout = 6;
+double duration, cm, inches;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(trigpin, OUTPUT);
+  pinMode(echopin, INPUT);
+  pinMode(speakerout, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigpin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigpin, LOW);
+
+  duration = pulseIn(echopin, HIGH);
+
+  cm = (duration / 2) / 29.1;
+
+  if (duration > 150 && duration < 500) {
+    tone(speakerout, NOTE_C4);
+  }
+  else if (duration > 500 && duration < 800) {
+    tone(speakerout, NOTE_G4);
+  }
+  else {
+    noTone(speakerout);
+  }
+
+  Serial.print(duration);
+  Serial.println(" duration");
+  Serial.print(cm);
+  Serial.print(" cm");
+  Serial.println();
+}
